1b2fdbeafe3d65fa220482a9e298c1eb1819905d
tuana <tuananh45468@gmail.com>
2023-05-01 15:10:25 +0700

    remove speech bubble when resize screen

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 const share = document.getElementById("share");
 const avatar = document.getElementById("avatar");
 const iconShare = document.getElementById("icon-share");
-const svgShare = iconShare.nextElementSibling;
+const svgShare = document.querySelector("#icon-share svg");
 const speechBubble = document.querySelector(".speech-bubble");
 
 const handleShareClickMobile = () => {
@@ -11,15 +11,31 @@ const handleShareClickMobile = () => {
 
 const handleShareClickLaptop = () => {
   speechBubble.classList.remove("hidden");
-  iconShare.classList.add("bg-grayish-blue");
+  iconShare.classList.add("bg-dark-grayish-blue");
+  svgShare.classList.remove("fill-dark-grayish-blue");
   svgShare.classList.add("fill-light-grayish-blue");
 };
 
-const mediaQuery = window.matchMedia("(max-width: 768px)");
+const mediaQuery = window.matchMedia("(max-width: 1439px)");
 
-if (mediaQuery.matches) {
-  iconShare.addEventListener("click", handleShareClickMobile);
-} else {
-  iconShare.removeEventListener("click", handleShareClickMobile);
-  iconShare.addEventListener("click", handleShareClickLaptop);
-}
+const handleResize = () => {
+  if (mediaQuery.matches) {
+    iconShare.classList.remove("bg-dark-grayish-blue");
+    iconShare.removeEventListener("click", handleShareClickLaptop);
+    iconShare.addEventListener("click", handleShareClickMobile);
+    speechBubble.classList.add("hidden");
+    iconShare.classList.remove("bg-grayish-blue");
+    svgShare.classList.remove("fill-light-grayish-blue");
+  } else {
+    share.classList.add("hidden");
+    avatar.classList.remove("hidden");
+    iconShare.removeEventListener("click", handleShareClickMobile);
+    iconShare.addEventListener("click", handleShareClickLaptop);
+  }
+};
+
+//Initial set up based on screen size
+handleResize();
+
+//Add resize listener
+window.addEventListener("resize", handleResize);
